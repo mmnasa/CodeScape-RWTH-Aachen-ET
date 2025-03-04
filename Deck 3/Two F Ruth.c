6e5503283b4cdc049f3771b7e6981a64b78e8231
xCore <mostafaalsayedali@gmail.com>
2025-03-04 17:28:37 +0100

    Update Two F Ruth.c

--- Deck 3/Two F Ruth.c
+++ Deck 3/Two F Ruth.c
@@ -1,29 +1,39 @@
 #include "codescape.h"
 #include <stdlib.h>
 #include <string.h>
+
 void run(char array[]) {
     int moves = 0;
+    int indexOfA = -1;  
+    int arrayLength = strlen(array);  
 
+    for (int i = 0; i < arrayLength; i++) {
+        if (array[i] == 'a') {
+            indexOfA = i;
+            break;  
+        }
+    }
     while (moves < 25) {
         move();
         moves++;
         if (moves == 2 || moves == 12) {
             turnLeft();
-            for (int i = 0; i < strlen(array); i++) {
-                if (array[i] == '\0') break;
-                char temp[1];
-                temp[0] = array[i];
-                temp[1] = '\0';
+
+            if (moves == 2 && indexOfA != -1) {
+                writeInt(indexOfA);
+            }
+            if (moves == 12) {
+                char temp[2] = { array[5], '\0' };
                 write(temp);
             }
             turnRight();
-        }else {
-            if (moves == 3 || moves == 11) {
-                turnLeft();
-            } else if ( moves == 7 || moves == 9) {
-                turnRight();
-                pickUp();
-            }
+        } 
+        else if (moves == 3 || moves == 11) {
+            turnLeft();
+        }
+        else if (moves == 7 || moves == 9) {
+            turnRight();
+            pickUp();
         }
     }
 }
