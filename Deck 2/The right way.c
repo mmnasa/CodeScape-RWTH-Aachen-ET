75a5d990c2478efe9af6fbb55c18c45926fb1a38
xCore <mostafaalsayedali@gmail.com>
2025-03-18 16:11:00 +0100

    Update The right way.c

--- Deck 2/The right way.c
+++ Deck 2/The right way.c
@@ -1,39 +1,31 @@
 #include "codescape.h"
+/**
+ * A parameter that is required to solve the room
+ * is passed on to the function run().
+ * nr Nummer des Teleporters, der zum Ausgang führt
+ */
 void run(int nr) {
-    for (int i=0;i<=1;i++){
-        move();
-    }
-    if (nr<=2){
-        turnLeft();
-    }else { 
-        turnRight();
-    }
-    for (int i=0;i<=2;i++){
-        move();
-    }
-    if (nr>2){
-        turnLeft();
-    }else { 
-        turnRight();
-    }
-    for (int i=0;i<=1;i++){
-        move();
-    }
-    if(nr%2==0){
-        turnRight();
-        for (int i=0;i<=1;i++){
-        move();
-    }
-    }else{
-        turnLeft();
-        for (int i=0;i<=1;i++){
-        move();
-        }
-        for (int i=0;i<=1;i++){
+    for (int i = 0; i < 11; i++) {
+        if ((nr <= 2 && i == 2) || (nr >= 3 && i == 5)) {
             turnLeft();
+        } else if (i == 2 || i == 5) {
+            turnRight();
         }
+        if (i == 7) {
+            if (nr % 2 == 0) {
+                turnRight();
+            } else {
+                turnLeft();
+            }
+        }
+        if (i == 9) {
+            while (!isMovePossible()) {
+                turnLeft();
+            }
+        }
+        if (i == 10) {
+            pickUp();
+        }
+        move();
     }
-        move();
-        pickUp();
-        move();
 }
